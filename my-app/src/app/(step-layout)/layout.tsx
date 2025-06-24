72daf7a92015de1c6f1c155ffa6f1b414a05363c
Jason <jrmoynihan99@gmail.com>
2025-06-24 12:34:50 -0400

    removing scroll bars by default on mobile test

--- my-app/src/app/(step-layout)/layout.tsx
+++ my-app/src/app/(step-layout)/layout.tsx
@@ -23,7 +23,7 @@ function LayoutContent({ children }: { children: React.ReactNode }) {
   const { isOpen } = useSidebar();
 
   return (
-    <div className="relative flex h-screen w-screen overflow-hidden bg-black text-white">
+    <div className="relative flex h-full w-full overflow-hidden bg-black text-white">
       <Sidebar />
 
       {/* Sticky MenuButton that sits above sidebar and content on mobile */}
